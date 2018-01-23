ab46ab6c9eabc6b629654f2a235356294af2ede0
Mosef Asad <mosefasad@gmail.com>
2018-01-23 16:48:47 -0500

    updated router get method

--- routes/lessons.router.js
+++ routes/lessons.router.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const passport = require('passport');
-const {Lesson}  = require('../models/lesson');
+const express = require("express");
+const passport = require("passport");
+const { Lesson } = require("../models/lesson");
 
-require('../auth/strategies')(passport);
+require("../auth/strategies")(passport);
 const router = express.Router();
 
-router.get('/GET', passport.authenticate("jwt", { session: false }), (req, res) => {
-  Lesson.find({ chapter })
-      .then(lessons => {
-        res.status(200).json({lessons});
+router.get("/GET", passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Lesson.findOne({ chapter })
+      .then(lesson => {
+        res.status(200).json({ lessons });
       })
       .catch(err => {
         res.status(400).json({ error: "something went terribly wrong" });
       });
-});
+  }
+);
 
 module.exports = { router };
