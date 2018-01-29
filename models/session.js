6472fe102281f8de04d31b3b89891338c50542b1
Mosef Asad <mosefasad@gmail.com>
2018-01-29 03:15:12 -0500

    updated session schema to include userId

--- models/session.js
+++ models/session.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const User = require("./user");
 
 const sessionSchema = new mongoose.Schema({
   startedAt: { type:Number },
   stoppedAt: { type:Number },
   durationInMs: { type:Number },
-  userId: { type: String },
+  userId: {type: Schema.Types.ObjectId, ref:'User'},
   score: { type:Number }
 });
 
